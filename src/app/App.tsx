11e8f7f39393209fc8f944fa53958af2e6ff5ae9
evrenaksuna13 <evrenaksuna13@gmail.com>
2026-07-09 20:15:35 +0300

    ..
    ..

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
       { path: "resources", element: <LearningCenterPage /> },
       { path: "resources/:slug", element: <ArticleDetailPage /> },
       { path: "about", element: <AboutPage /> },
-      { path: "resources/WhatAreMetalRoofTiles", element: <WhatAreMetalRoofTiles /> },
+      { path: "resources", element: <WhatAreMetalRoofTiles /> },
       { path: "/videos", element: <Videos /> },
       { path: "about/sustainability", element: <SustainabilityPage /> },
       { path: "about/warranty", element: <WarrantyPage /> },
