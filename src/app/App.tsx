63747cdcb2a6653da31328fcc2e5c48421ad70d1
evrenaksuna13 <evrenaksuna13@gmail.com>
2026-07-09 19:53:41 +0300

    importduzeltildi

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
       { path: "resources", element: <LearningCenterPage /> },
       { path: "resources/:slug", element: <ArticleDetailPage /> },
       { path: "about", element: <AboutPage /> },
-      { path: "/whataremetalrooftiles", element: <WhatAreMetalRoofTiles /> },
+      { path: "/WhatAreMetalRoofTiles", element: <WhatAreMetalRoofTiles /> },
       { path: "/videos", element: <Videos /> },
       { path: "about/sustainability", element: <SustainabilityPage /> },
       { path: "about/warranty", element: <WarrantyPage /> },
